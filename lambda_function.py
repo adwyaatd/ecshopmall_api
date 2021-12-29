4d673ff89368c3bab26f4c97c17727051805d1ea
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-12-29 21:57:51 +0900

    pep8に準拠

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,28 @@
 import boto3
-from boto3.dynamodb.conditions import Key,Attr
+from boto3.dynamodb.conditions import Key, Attr
 
 
 def main():
-  dynamoDB = boto3.resource("dynamodb")
-  table = dynamoDB.Table("ESM")
-  query_data = table.query(
-    IndexName='GSI1',
-    KeyConditionExpression = Key("SK").eq("shop") & Key("PK").begins_with("shop_")
-  )
-  print(query_data)
-  shops = query_data["Items"]
-  # print(shops)
-  return shops
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("ESM")
+    query_data = table.query(
+        IndexName='GSI1',
+        KeyConditionExpression=Key("SK").eq(
+            "shop") & Key("PK").begins_with("shop_")
+    )
+    print(query_data)
+    shops = query_data["Items"]
+    # print(shops)
+    return shops
 
 
 def lambda_handler(event, context):
-  try:
-    shops = main()
-    response = {
-      "statuscode": 200,
-      "data": shops
-    }
-    return response
-  except Exception as e:
-    print("Error")
+    try:
+        shops = main()
+        response = {
+            "statuscode": 200,
+            "data": shops
+        }
+        return response
+    except Exception as e:
+        print("Error")
